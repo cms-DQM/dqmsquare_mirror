67edba589b972961738bd8216b670420f3dc9a55
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-09-08 13:26:37 +0200

    Cleanup, remove unused files

--- dqmsquare_k8_wrapper.sh
+++ dqmsquare_k8_wrapper.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Wrapper script for launching the Mirror on a kubernetes pod. 
+# Accepts one argument, the type of service to run. Can be "server", "dummy".
+# Launches two python grabbers in the background, and then runs the flash app with
+# gunicorn.
+
 service=$1
 
-#sudo mkdir -p /cephfs/testbed/dqmsquare_mirror/db/
-#sudo mkdir -p /cephfs/testbed/dqmsquare_mirror/tmp/
-#sudo mkdir -p /cephfs/testbed/dqmsquare_mirror/log/
-#sudo find /cephfs/testbed/dqmsquare_mirror -type d -exec chmod 777 {} \;
+LOGS_DIR=/cinder/dqmsquare/log/
+PVC_DIR=/cinder/dqmsquare
 
-sudo mkdir -p /cinder/dqmsquare/log/
-sudo find /cinder/dqmsquare -type d -exec chmod 777 {} \;
+echo Creating logs dir $LOGS_DIR ...
+sudo mkdir -p $LOGS_DIR
+
+echo Changing permissions for PVC claim $PVC_DIR ...
+sudo find  -type d -exec chmod 777 {} \;
+
 
 python3 dqmsquare_cfg.py k8
 
@@ -16,12 +23,13 @@ if [[ $service ]] && [[ $service = "server" ]] ; then
   python3 grabber.py production & 
   python3 grabber.py playback & 
 
+  # Launch gunicorn with 4 workers.
   gunicorn -w 4 -b 0.0.0.0:8084 'wsgi:flask_app'
 fi
 
 if [[ $service ]] && [[ $service = "dummy" ]] ; then
   while true; do
-	  echo "Sleep ..."
+	  echo "Sleeping ..."
 	  sleep 60
   done
 fi
