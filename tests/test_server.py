68c23c9ffbc79143074764052b74fef2a33b2513
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-06-05 18:24:07 +0200

    More layers for dockerfile for better caching, cleanup files in dockerignore, fix tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,7 +3,7 @@ from flask.testing import FlaskClient
 
 # Tests to check Flask server and its responses
 # Needed to be passed as a fixture
-from fixtures import app, cfg, client, testing_databases, testing_database
+from fixtures import app, dqm2_config, client, testing_databases, testing_database
 
 
 def test_server_home(client: FlaskClient):
@@ -39,8 +39,15 @@ def test_server_get_run(client: FlaskClient):
     assert response.status_code == 200
     response = json.loads(response.text)
     assert isinstance(response, list)
-    assert response[0][0] == 377187
+    assert response[0][0] == 358792
     assert response[0][1] == None
+
+    response = client.get("/api?what=get_run&run=358791&db=production")
+    assert response.status_code == 200
+    response = json.loads(response.text)
+    assert isinstance(response, list)
+    assert response[0][0] == 358788
+    assert response[0][1] == 358792
 
 
 def test_server_get_clients(client: FlaskClient):
@@ -57,9 +64,10 @@ def test_server_get_clients(client: FlaskClient):
     assert response.status_code == 400
 
     # Proper request
-    response = client.get("/api?what=get_clients&from=377187&to=381574&db=production")
+    response = client.get("/api?what=get_clients&from=358788&to=358792&db=production")
     assert response.status_code == 200
     response = json.loads(response.text)
+    print(response)
     assert isinstance(response, list)
     assert response[0] == "beam"
-    assert response[-1] == "visualization-live"
+    assert response[-1] == "visualization-live-secondInstance"
